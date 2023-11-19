8c025be467d6e80628f4679729d2da02af3557fb
Yohni <62948056+HRyohni@users.noreply.github.com>
2023-11-19 01:30:40 +0100

    commit

---
+++ BombGame/src/main.js
@@ -0,0 +1,32 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+import axios from "axios";
+
+const app = createApp(App)
+const port = 3000;
+const dataServiceBaseUrl = `http://127.0.0.1:${port}`;
+
+
+app.use(router)
+
+
+
+
+
+
+// Vuetify
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+app.use(vuetify)
+app.mount('#app')
---
+++ BombGame/src/views/Hellout.vue
@@ -0,0 +1,65 @@
+<script>
+// imports
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    //components
+  },
+
+  data: () => ({
+    username: "",
+    email: "",
+    password: ""
+    //
+
+  }),
+  async mounted() {
+
+  },
+
+  methods: {
+
+    async addData() {
+      try {
+        const res = await axios.post("http://localhost:3000/api/user/create", {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+
+        }
+
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+  },
+};
+</script>
+
+<template>
+  <v-container>
+    <v-card class="pa-3">
+
+      <v-card-text>username</v-card-text>
+      <v-text-field v-model="username" ></v-text-field>
+
+      <v-card-text>email</v-card-text>
+      <v-text-field  v-model="email"></v-text-field>
+
+      <v-card-text>password</v-card-text>
+      <v-text-field  v-model="password"></v-text-field>
+
+
+      <v-btn @click="addData()" class="pa-2">submit</v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
